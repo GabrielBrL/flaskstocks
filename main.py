eabb6febc500610c0360671e3845d8bc4719f615
Gabriel Barros <gabriellourencobarros02@gmail.com>
2024-01-23 21:43:09 -0300

    Alterando projeto

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import fundamentus
 from flask import Flask, jsonify
 import os
 
@@ -7,6 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+@app.route("/<key>")
+def hello_world(key):
+    if(key == "gab2020"):
+        df = fundamentus.get_resultado()
+        dictionary = df.to_dict(orient='index')
+        return dictionary
+    else:
+        return "Não autorizado"
 
 
 if __name__ == '__main__':
