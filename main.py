92e84e90f99cd406ffb9245e928e76ff8c007cf0
Gabriel Barros <gabriellourencobarros02@gmail.com>
2024-01-25 07:13:02 -0300

    feat:Adicionada rota para ter detalhes do papel

--- main.py
+++ main.py
@@ -8,12 +8,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "eu te amo MUITO linda hehe <3"
+    return "acesso restrito"
 
 @app.route("/<key>")
 def hello_world(key):
     if(key == "gab2020"):
-        df = fundamentus.get_resultado()
+        df = fundamentus.get_resultado()    
+        dictionary = df.to_dict(orient='index')
+        json_resultado = jsonify(dictionary)
+        return json_resultado
+    else:
+        return "Não autorizado"
+    
+@app.route("/<key>/<papel>")
+def get_detalhes(key, papel):
+    if(key == "gab2020"):
+        df = fundamentus.get_detalhes_papel(papel)
         dictionary = df.to_dict(orient='index')
         json_resultado = jsonify(dictionary)
         return json_resultado
